c4a90f382dbedd97e260c30269ee979202f31706
Josasp <josasp.asp@gmail.com>
2013-06-07 11:11:02 +0200

    Städade upp lite GolfReception

--- Golf/Golf/GolfReception.cs
+++ Golf/Golf/GolfReception.cs
@@ -59,35 +59,7 @@ namespace Golf
 
         private void golfreception_Load(object sender, EventArgs e)
         {
-            // Bara lite test... 
 
-            listView1.View = View.SmallIcon; // SmallIcon / LargeIcon
-            //listView1.CheckBoxes = true;
-
-            string[] tid = { "08:00", "08:10", "08:20", "08:30", "08:40", "08.50", "09:00", "09:10", "09:20" }; //osv. 
-
-            int raknare = 0; 
-
-            while (raknare < 9) 
-            {
-                // if tid finns i databasen, lägg i item antal spelare bokade = prickar.
-                listView1.Items.Add(tid[raknare]);
-
-                raknare = raknare + 1; 
-            }
-
-
-
-            //Add the items to the ListView.
-            /*ListViewItem item1 = new ListViewItem("item1", 0);
-            ListViewItem item2 = new ListViewItem("item2", 0);
-            ListViewItem item3 = new ListViewItem("item3", 0);
-            listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3 });*/
-
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
         }
 
         private void tidsbokningToolStripMenuItem_Click(object sender, EventArgs e)
--- Golf/Golf/golfreception.Designer.cs
+++ Golf/Golf/golfreception.Designer.cs
@@ -37,7 +37,6 @@
             this.tävlingsverksamhetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.registreraTävlingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sökTävlingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,7 +48,7 @@
             this.tävlingsverksamhetToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(635, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1012, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -114,24 +113,11 @@
             this.sökTävlingToolStripMenuItem.Text = "Sök tävling";
             this.sökTävlingToolStripMenuItem.Click += new System.EventHandler(this.sökTävlingToolStripMenuItem_Click);
             // 
-            // listView1
-            // 
-            this.listView1.LabelWrap = false;
-            this.listView1.Location = new System.Drawing.Point(55, 76);
-            this.listView1.MultiSelect = false;
-            this.listView1.Name = "listView1";
-            this.listView1.ShowItemToolTips = true;
-            this.listView1.Size = new System.Drawing.Size(505, 249);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
-            // 
             // GolfReception
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 512);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(1012, 766);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "GolfReception";
@@ -155,7 +141,6 @@
         private System.Windows.Forms.ToolStripMenuItem tävlingsverksamhetToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem registreraTävlingToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sökTävlingToolStripMenuItem;
-        private System.Windows.Forms.ListView listView1;
 
     }
 }
