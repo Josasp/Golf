db6f48ba898fd455b310c0ff384f85c850240e04
Jennifer Bergne <j.bergne@telia.com>
2013-06-09 01:06:36 +0200

    se alla ändra medlem - halvklar

--- Golf/Golf/SeAlla.cs
+++ Golf/Golf/SeAlla.cs
@@ -72,7 +72,7 @@ namespace Golf
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            /*
             int nummer = dataGridView1.CurrentRow.Index; 
             MessageBox.Show(Convert.ToString(nummer));
 
@@ -93,16 +93,84 @@ namespace Golf
 
             drg.Close();
 
-            MessageBox.Show(idet);
+            MessageBox.Show(idet);*/
 
 
-            String sql2 = "delete from medlem where golf_id = '" + idet + "';";
-            NpgsqlCommand command4 = new NpgsqlCommand(sql2, GolfReception.conn);
-            command4.ExecuteNonQuery();
+           // String sql2 = "delete from medlem where golf_id = '" + idet + "';";
+            //NpgsqlCommand command4 = new NpgsqlCommand(sql2, GolfReception.conn);
+            //command4.ExecuteNonQuery();
+
+            uppdateralista();
+
+        }
+
+        public string hamtaid()
+        {
+            int nummer = dataGridView1.CurrentRow.Index;
+
+            String sql = "select golf_id from medlem;";
+            NpgsqlCommand command3 = new NpgsqlCommand(sql, GolfReception.conn);
+            NpgsqlDataReader drg = command3.ExecuteReader();
+
+            string idet = "";
+            int raknare = 0;
+            while (drg.Read())
+            {
+                if (raknare == nummer)
+                {
+                    idet = drg["golf_id"].ToString();
+                }
+                raknare = raknare + 1;
+            }
+
+            drg.Close();
+
+            return idet; 
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            LaggTillspelare laggtillspelare = new LaggTillspelare();
+            laggtillspelare.ShowDialog();
 
             uppdateralista(); 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string id = hamtaid(); 
+            String sql3 = "select    medlem.golf_id,     medlem.\"förnamn\",     medlem.efternamn,   medlem.handicap,    status.namn,   medlem.\"betaltÅr\",      medlem.adress,      medlem.stad,    medlem.postnummer,      medlem.telefonnummer,    medlem.epost,   medlem.\"kön_id\"    from    medlem, status     where    medlem.status_id = status.status_id      and     medlem.golf_id ='" + id + "';";
+            NpgsqlCommand command5 = new NpgsqlCommand(sql3, GolfReception.conn);
+            NpgsqlDataReader drx = command5.ExecuteReader();
+
+            while (drx.Read())
+            {
+                golfid_textBox.Text = drx["golf_id"].ToString();
+                fornamn_textBox.Text = drx["förnamn"].ToString();
+                efternamn_textBox.Text = drx["efternamn"].ToString();
+                handicap_textBox.Text = drx["handicap"].ToString();
+                betalt_textBox.Text = drx["betaltÅr"].ToString();
+                adress_textBox.Text = drx["adress"].ToString();
+                postnummer_textBox.Text = drx["postnummer"].ToString();
+                postort_textBox.Text = drx["stad"].ToString();
+                telefonnummer_textBox.Text = drx["telefonnummer"].ToString();
+                epost_textBox.Text = drx["epost"].ToString();
+                medlemsstatus_comboBox.Text = drx["namn"].ToString();
+
+                if (drx["kön_id"].ToString() == "0")
+                {
+                    kon_comboBox.Text = "Man"; 
+                }
+                else if (drx["kön_id"].ToString() == "1")
+                {
+                    kon_comboBox.Text = "Kvinna"; 
+                }
+                    
+            }
+            drx.Close();
 
 
+            groupBox1.Visible = true; 
         }
 
 
--- Golf/Golf/sealla.Designer.cs
+++ Golf/Golf/sealla.Designer.cs
@@ -31,12 +31,41 @@
             this.ok_button = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.betalt_textBox = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.medlemsstatus_comboBox = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.kon_comboBox = new System.Windows.Forms.ComboBox();
+            this.handicap_textBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.postort_textBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.postnummer_textBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.adress_textBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.epost_textBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.telefonnummer_textBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fornamn_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.efternamn_textBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.golfid_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ok_button
             // 
-            this.ok_button.Location = new System.Drawing.Point(951, 325);
+            this.ok_button.Location = new System.Drawing.Point(869, 342);
             this.ok_button.Name = "ok_button";
             this.ok_button.Size = new System.Drawing.Size(75, 23);
             this.ok_button.TabIndex = 55;
@@ -49,24 +78,296 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(741, 307);
+            this.dataGridView1.Size = new System.Drawing.Size(661, 316);
             this.dataGridView1.TabIndex = 57;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 326);
+            this.button1.Location = new System.Drawing.Point(126, 334);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(97, 23);
             this.button1.TabIndex = 58;
-            this.button1.Text = "Radera medlem";
+            this.button1.Text = "Radera markerad";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // betalt_textBox
+            // 
+            this.betalt_textBox.Location = new System.Drawing.Point(63, 281);
+            this.betalt_textBox.Name = "betalt_textBox";
+            this.betalt_textBox.Size = new System.Drawing.Size(77, 20);
+            this.betalt_textBox.TabIndex = 82;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(9, 285);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 13);
+            this.label9.TabIndex = 81;
+            this.label9.Text = "Betalt år: ";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(24, 133);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(37, 13);
+            this.label12.TabIndex = 80;
+            this.label12.Text = "Status";
+            // 
+            // medlemsstatus_comboBox
+            // 
+            this.medlemsstatus_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.medlemsstatus_comboBox.FormattingEnabled = true;
+            this.medlemsstatus_comboBox.Items.AddRange(new object[] {
+            "Aktiv",
+            "Vilande",
+            "Greenfee",
+            "Junior",
+            "Ickemedlem"});
+            this.medlemsstatus_comboBox.Location = new System.Drawing.Point(63, 128);
+            this.medlemsstatus_comboBox.Name = "medlemsstatus_comboBox";
+            this.medlemsstatus_comboBox.Size = new System.Drawing.Size(147, 21);
+            this.medlemsstatus_comboBox.TabIndex = 75;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(35, 109);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 13);
+            this.label11.TabIndex = 79;
+            this.label11.Text = "Kön";
+            // 
+            // kon_comboBox
+            // 
+            this.kon_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.kon_comboBox.FormattingEnabled = true;
+            this.kon_comboBox.Items.AddRange(new object[] {
+            "Man",
+            "Kvinna"});
+            this.kon_comboBox.Location = new System.Drawing.Point(63, 104);
+            this.kon_comboBox.Name = "kon_comboBox";
+            this.kon_comboBox.Size = new System.Drawing.Size(75, 21);
+            this.kon_comboBox.TabIndex = 78;
+            // 
+            // handicap_textBox
+            // 
+            this.handicap_textBox.Location = new System.Drawing.Point(63, 32);
+            this.handicap_textBox.Name = "handicap_textBox";
+            this.handicap_textBox.Size = new System.Drawing.Size(57, 20);
+            this.handicap_textBox.TabIndex = 77;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(8, 36);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 13);
+            this.label10.TabIndex = 76;
+            this.label10.Text = "Handicap";
+            // 
+            // postort_textBox
+            // 
+            this.postort_textBox.Location = new System.Drawing.Point(63, 206);
+            this.postort_textBox.Name = "postort_textBox";
+            this.postort_textBox.Size = new System.Drawing.Size(108, 20);
+            this.postort_textBox.TabIndex = 74;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 73;
+            this.label8.Text = "Stad";
+            // 
+            // postnummer_textBox
+            // 
+            this.postnummer_textBox.Location = new System.Drawing.Point(63, 180);
+            this.postnummer_textBox.Name = "postnummer_textBox";
+            this.postnummer_textBox.Size = new System.Drawing.Size(108, 20);
+            this.postnummer_textBox.TabIndex = 72;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 184);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 71;
+            this.label7.Text = "Postnr";
+            // 
+            // adress_textBox
+            // 
+            this.adress_textBox.Location = new System.Drawing.Point(63, 155);
+            this.adress_textBox.Name = "adress_textBox";
+            this.adress_textBox.Size = new System.Drawing.Size(160, 20);
+            this.adress_textBox.TabIndex = 70;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 69;
+            this.label6.Text = "Address";
+            // 
+            // epost_textBox
+            // 
+            this.epost_textBox.Location = new System.Drawing.Point(63, 256);
+            this.epost_textBox.Name = "epost_textBox";
+            this.epost_textBox.Size = new System.Drawing.Size(160, 20);
+            this.epost_textBox.TabIndex = 68;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 67;
+            this.label5.Text = "E-post";
+            // 
+            // telefonnummer_textBox
+            // 
+            this.telefonnummer_textBox.Location = new System.Drawing.Point(63, 231);
+            this.telefonnummer_textBox.Name = "telefonnummer_textBox";
+            this.telefonnummer_textBox.Size = new System.Drawing.Size(160, 20);
+            this.telefonnummer_textBox.TabIndex = 66;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 65;
+            this.label4.Text = "Telefonnr";
+            // 
+            // fornamn_textBox
+            // 
+            this.fornamn_textBox.Location = new System.Drawing.Point(63, 56);
+            this.fornamn_textBox.Name = "fornamn_textBox";
+            this.fornamn_textBox.Size = new System.Drawing.Size(147, 20);
+            this.fornamn_textBox.TabIndex = 64;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 63;
+            this.label3.Text = "Efternamn";
+            // 
+            // efternamn_textBox
+            // 
+            this.efternamn_textBox.Location = new System.Drawing.Point(63, 80);
+            this.efternamn_textBox.Name = "efternamn_textBox";
+            this.efternamn_textBox.Size = new System.Drawing.Size(147, 20);
+            this.efternamn_textBox.TabIndex = 62;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 61;
+            this.label2.Text = "Förnamn";
+            // 
+            // golfid_textBox
+            // 
+            this.golfid_textBox.Location = new System.Drawing.Point(63, 9);
+            this.golfid_textBox.Name = "golfid_textBox";
+            this.golfid_textBox.Size = new System.Drawing.Size(147, 20);
+            this.golfid_textBox.TabIndex = 60;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 59;
+            this.label1.Text = "Golf-ID";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.betalt_textBox);
+            this.groupBox1.Controls.Add(this.golfid_textBox);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.epost_textBox);
+            this.groupBox1.Controls.Add(this.postort_textBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.medlemsstatus_comboBox);
+            this.groupBox1.Controls.Add(this.telefonnummer_textBox);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.postnummer_textBox);
+            this.groupBox1.Controls.Add(this.handicap_textBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.adress_textBox);
+            this.groupBox1.Controls.Add(this.kon_comboBox);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.fornamn_textBox);
+            this.groupBox1.Controls.Add(this.efternamn_textBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(696, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 316);
+            this.groupBox1.TabIndex = 83;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ändra ";
+            this.groupBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(167, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 84;
+            this.button2.Text = "Spara";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(229, 334);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 23);
+            this.button3.TabIndex = 84;
+            this.button3.Text = "Ändra markerad";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 334);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 23);
+            this.button4.TabIndex = 85;
+            this.button4.Text = "Lägg till ny spelare";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // SeAlla
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1038, 360);
+            this.ClientSize = new System.Drawing.Size(959, 377);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ok_button);
@@ -75,6 +376,8 @@
             this.Text = "Se alla";
             this.Load += new System.EventHandler(this.sealla_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -84,5 +387,33 @@
         private System.Windows.Forms.Button ok_button;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox betalt_textBox;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.ComboBox medlemsstatus_comboBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox kon_comboBox;
+        private System.Windows.Forms.TextBox handicap_textBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox postort_textBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox postnummer_textBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox adress_textBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox epost_textBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox telefonnummer_textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox fornamn_textBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox efternamn_textBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox golfid_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
